5550ed90d427e2ee3b828297944ef16a316fff70
Kannan <kannnaravinther@gmail.com>
2021-11-10 10:21:36 +0800

    First commit

---
+++ lib/auth/firebase_user_provider.dart
@@ -0,0 +1,17 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:rxdart/rxdart.dart';
+
+class ChatFirebaseUser {
+  ChatFirebaseUser(this.user);
+  final User user;
+  bool get loggedIn => user != null;
+}
+
+ChatFirebaseUser currentUser;
+bool get loggedIn => currentUser?.loggedIn ?? false;
+Stream<ChatFirebaseUser> chatFirebaseUserStream() => FirebaseAuth.instance
+    .authStateChanges()
+    .debounce((user) => user == null && !loggedIn
+        ? TimerStream(true, const Duration(seconds: 1))
+        : Stream.value(user))
+    .map<ChatFirebaseUser>((user) => currentUser = ChatFirebaseUser(user));
